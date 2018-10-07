7641612e511ec78e750600eaddc247b8db12118d
selenasmall <selenawiththetattoo@gmail.com>
2018-10-07 11:41:57 +1100

    React snapshot test
    Run React Tests
     cd client && yarn test
    
    Creating a snapshot
    expect shallow render and run the test
    
    Update snapshot
    edit snapshot file, run test, see it fail, update by running $u

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+import { shallowToJson } from 'enzyme-to-json';
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('snapshot testing', () => {
+  const output = shallow(<App />)
+  expect(shallowToJson(output)).toMatchSnapshot()
+})
