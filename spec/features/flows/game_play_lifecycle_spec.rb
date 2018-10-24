186e3810bfc6db57983eb4187049481abf9d6acf
selenasmall <selenawiththetattoo@gmail.com>
2018-10-24 13:26:25 +1100

    test context using data set up in the test db

--- spec/features/flows/game_play_lifecycle_spec.rb
+++ spec/features/flows/game_play_lifecycle_spec.rb
@@ -1,27 +1,33 @@
 require 'rails_helper'
 
 feature 'Game play lifecycle', js: true do
-  scenario 'WINNING a game' do
-    When 'user starts the game' do
-      visit('/')
-      wait_for { focus_on(:welcome).text }.to eq('Welcome to the Game')
-
-      focus_on(:welcome).start_game
+  context 'the game terms are configured' do
+    before do
+      Term.create(phrase: 'tequila')
     end
 
-    Then 'the game commences' do
-      wait_for { focus_on(:game).round }.to eq('1')
-      wait_for { focus_on(:game).status }.to eq('IN-PROGRESS')
-      wait_for { focus_on(:game).images.count }.to eq(9)
-    end
+    scenario 'WINNING a game' do
+      When 'user starts the game' do
+        visit('/')
+        wait_for { focus_on(:welcome).text }.to eq('Welcome to the Game')
 
-    When 'the user submits an answer' do
-      focus_on(:game).fill_guess('tequila')
-    end
+        focus_on(:welcome).start_game
+      end
 
-    Then 'the user wins' do
-      pending
-      wait(0.5).for { focus_on(:game).status }.to eq('WINNER')
+      Then 'the game commences' do
+        wait_for { focus_on(:game).round }.to eq('1')
+        wait_for { focus_on(:game).status }.to eq('IN-PROGRESS')
+        wait_for { focus_on(:game).images.count }.to eq(9)
+      end
+
+      When 'the user submits an answer' do
+        focus_on(:game).fill_guess('tequila')
+      end
+
+      Then 'the user wins' do
+        pending
+        wait(0.5).for { focus_on(:game).status }.to eq('WINNER')
+      end
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -45,6 +45,7 @@ Capybara.register_driver :selenium_chrome do |app|
   )
 end
 Capybara.javascript_driver = :selenium_chrome
+Capybara.server_port = 3001
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
