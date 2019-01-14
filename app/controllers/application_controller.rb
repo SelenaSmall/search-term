f7389d5b5537cc5832af589d72163042fc7a6536
selenasmall <selenawiththetattoo@gmail.com>
2019-01-15 09:11:23 +1100

    defined? does not work here

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def pause_execution
     return if Rails.env.production?
-    return unless defined? Rails.application.config.should_pause
+    return unless Rails.application.config.respond_to?(:should_pause)
 
     # pause everything, may want to make this granular per call
     sleep 0.2 while Rails.application.config.should_pause
