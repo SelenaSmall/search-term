67c7627323370783958b66d9c0a857451f4901df
selenasmall <selenawiththetattoo@gmail.com>
2019-01-15 13:53:41 +1100

    Comment wrapping div on header component for demo

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,22 +1,24 @@
 import React from 'react';
 
 export default ({ status, score, secondsRemaining }) => (
-  <section id="top-region">
-    <menu className="status-menu">
-      <ul>
-        <li className="status-menu-item status">
-          <span className="status-menu-item-label">Status</span>
-          <span className="status-menu-item-value">{status}</span>
-        </li>
-        <li className="status-menu-item score">
-          <span className="status-menu-item-label">Score</span>
-          <span className="status-menu-item-value">{score}</span>
-        </li>
-        <li className="status-menu-item timer">
-          <span className="status-menu-item-label">Time</span>
-          <span className="status-menu-item-value">{secondsRemaining}</span>
-        </li>
-      </ul>
-    </menu>
-  </section>
+// <div class="hidden">
+   <section id="top-region">
+     <menu className="status-menu">
+       <ul>
+         <li className="status-menu-item status">
+           <span className="status-menu-item-label">Status</span>
+           <span className="status-menu-item-value">{status}</span>
+         </li>
+         <li className="status-menu-item score">
+           <span className="status-menu-item-label">Score</span>
+           <span className="status-menu-item-value">{score}</span>
+         </li>
+         <li className="status-menu-item timer">
+           <span className="status-menu-item-label">Time</span>
+           <span className="status-menu-item-value">{secondsRemaining}</span>
+         </li>
+       </ul>
+     </menu>
+   </section>
+// </div>
 );
