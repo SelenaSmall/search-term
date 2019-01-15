5d251ad08ccd0681f833d732130c384f3dc1183d
Michael Milewski <saramic@gmail.com>
2019-01-16 04:07:49 +1100

    there is no get_term as it is now via the game

--- spec/models/term_spec.rb
+++ spec/models/term_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 describe Term do
-  it 'returns null if there are no terms in the database' do
-    expect(Term).to receive(:count).and_return(0)
-    expect(Term.get_term(1)).to eq nil
-  end
+  # it 'returns null if there are no terms in the database' do
+  #   expect(Term).to receive(:count).and_return(0)
+  #   expect(Term.get_term(1)).to eq nil
+  # end
 
   context '1 term in the database' do
     let(:mock_term) { double('term') }
@@ -14,17 +14,17 @@ describe Term do
       allow(Term).to receive_message_chain('all.order').with(:created_at).and_return([mock_term])
     end
 
-    it 'returns the term for round 0' do
-      expect(Term.get_term(0)).to eq mock_term
-    end
-
-    it 'returns the term for round 1' do
-      expect(Term.get_term(1)).to eq mock_term
-    end
-
-    it 'returns the term for round "1" passed as string' do
-      expect(Term.get_term('1')).to eq mock_term
-    end
+    # it 'returns the term for round 0' do
+    #   expect(Term.get_term(0)).to eq mock_term
+    # end
+    #
+    # it 'returns the term for round 1' do
+    #   expect(Term.get_term(1)).to eq mock_term
+    # end
+    #
+    # it 'returns the term for round "1" passed as string' do
+    #   expect(Term.get_term('1')).to eq mock_term
+    # end
   end
 
   context '3 terms in the database' do
@@ -38,20 +38,20 @@ describe Term do
       allow(Term).to receive_message_chain('all.order').with(:created_at).and_return(terms)
     end
 
-    it 'gets the modulus 1st term' do
-      expect(Term.get_term(1)).to eq mock_term_1
-    end
-
-    it 'gets the modulus 2nd term' do
-      expect(Term.get_term(2)).to eq mock_term_2
-    end
-
-    it 'gets the modulus 3rd term' do
-      expect(Term.get_term(3)).to eq mock_term_3
-    end
-
-    it 'gets the modulus 99th term' do
-      expect(Term.get_term(99)).to eq mock_term_3
-    end
+    # it 'gets the modulus 1st term' do
+    #   expect(Term.get_term(1)).to eq mock_term_1
+    # end
+    #
+    # it 'gets the modulus 2nd term' do
+    #   expect(Term.get_term(2)).to eq mock_term_2
+    # end
+    #
+    # it 'gets the modulus 3rd term' do
+    #   expect(Term.get_term(3)).to eq mock_term_3
+    # end
+    #
+    # it 'gets the modulus 99th term' do
+    #   expect(Term.get_term(99)).to eq mock_term_3
+    # end
   end
 end
