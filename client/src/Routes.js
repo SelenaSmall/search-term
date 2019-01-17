d5af790b5971b94b19874dbf02947523024821b9
Michael Milewski <saramic@gmail.com>
2019-01-17 15:07:13 +1100

    gun animation component

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 import Game from './components/Game';
 import Round from './components/Round';
 import Results from './components/Results';
+import Airport from './components/Airport';
 
 export default () => (
   <Switch>
     <Route exact path="/" component={Game} />
     <Route exact path="/game/:gameName/round/:round" component={Round} />
     <Route exact path="/results" component={Results} />
+    <Route exact path="/airport" component={Airport} />
   </Switch>
 );
---
+++ client/src/components/Airport/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import './Airport.css';
+
+export default class Airport extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { gun: 'h-1' };
+    this.loadGun = this.loadGun.bind(this);
+  }
+
+  loadGun() {
+    this.setState({ gun: 'h-32' });
+  }
+
+  render() {
+    const { gun } = this.state;
+    return (
+      <div>
+        <section id="middle-region">
+
+        <div className="game-main-container text-center">
+        <div className="flex -mx-2">
+          <section id="middle-region" className="flex-1 w-1/4 px-2">
+            <div>
+              <strong className="text-5xl">ARE YOU CARRYING ANY GUNS?</strong>
+            </div>
+            <div>
+              <img src="/gun_icon.png" id="gun" className={gun} />
+            </div>
+            <ul>
+              <li className="list-reset">
+                <button className="bg-red hover:bg-red-dark text-white font-bold py-2 px-4 rounded min-w-full mb-1 h-32"
+                        value="YES">YES
+                </button>
+                <button id="no"
+                        className="bg-red hover:bg-red-dark text-white font-bold py-2 px-4 rounded min-w-full mb-1 h-32"
+                        onMouseEnter={this.loadGun}
+                        value="NO">NO
+                </button>
+              </li>
+            </ul>
+          </section>
+        </div>
+      </div>
+        </section>
+      </div>
+    );
+  }
+};
