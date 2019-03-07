2378873e6e2207d6c676bd13b2a11990f1eb3d15
Michael Milewski <saramic@gmail.com>
2019-03-08 00:57:44 +1100

    basic formatting on .eslintrc file

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,15 +1,17 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "extends": "airbnb",
-  "globals": {
-    "navigator": false,
-    "window": false,
-    "document": false,
-    "expect": false,
-    "it": false,
+  parser: 'babel-eslint',
+  extends: 'airbnb',
+  globals: {
+    navigator: false,
+    window: false,
+    localStorage: false,
+    fetch: false,
+    document: false,
+    expect: false,
+    it: false
   },
-  "rules": {
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-    "react/prop-types": 0,
-  },
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/prop-types': 0
+  }
 };
